0fa8ad8fb93aa7a7fb67192f47d9d0ab7419f7e5
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-26 17:08:21 +0530

    [vishal] Create table in Payroll Service

---
+++ Uc2_Sql_CreateTable_PayrolService.sql
@@ -0,0 +1,5 @@
+-- uc1 creating database
+create database Employee_payroll;
+
+--uc 2 creating Employee_Payroll1
+create table Employee_Payroll1( id_num int IDENTITY(1,1) primary key, Name varchar(20),Sallary float,StartDate date);
